1f217c721fbc767b39abe4d2664309f27e776630
pavan-santhosh <pavansanthosh24042002@gmail.com>
2024-03-03 18:45:52 +0530

    Initial Commit

---
+++ realtime-chat-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <h1 className="text-3xl font-bold text-red-500 underline">
+          Hello world!
+        </h1>
+      </div>
+    </main>
+  );
+}

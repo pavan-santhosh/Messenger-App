15e7ecac5b228beb4d7a20498e5e33c53c46c1a8
pavan-santhosh <pavansanthosh24042002@gmail.com>
2024-03-07 14:37:35 +0530

    Database Setup with comments

--- realtime-chat-app/src/app/page.tsx
+++ realtime-chat-app/src/app/page.tsx
@@ -1,7 +1,7 @@
 import {db} from '@/lib/db'
 
 export default async function Home() {
-  await db.set('hello','') // it sets the key and value
+  await db.set('hello','super') // it sets the key and value under Data Browser
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">

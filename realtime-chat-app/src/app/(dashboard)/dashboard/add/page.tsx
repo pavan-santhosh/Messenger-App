1ded1a874e6c0f72da33ef14f09966fd3f7e014b
Pavan Santhosh <pavansanthosh24042002@gmail.com>
2024-07-28 15:32:49 +0530

    "Mobile View"

--- realtime-chat-app/src/app/(dashboard)/dashboard/add/page.tsx
+++ realtime-chat-app/src/app/(dashboard)/dashboard/add/page.tsx
@@ -1,13 +1,13 @@
 import AddFriendButton from "@/components/ui/AddFriendButton";
 import { FC } from "react";
- 
-const page : FC = () => {
+
+const page: FC = () => {
     return (
-        <main className="">
-            <h1 className="font-bold text-5xl mb-8 mt-8">Add a friend</h1>
-            <AddFriendButton/>
+        <main className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="font-bold text-5xl mb-8 -mt-8">Add a friend</h1>
+            <AddFriendButton />
         </main>
     )
 }
- 
+
 export default page;
